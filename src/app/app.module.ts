bc300cbc976b45e8ec48b3b8eb4fa1780553af9d
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-09-06 16:55:17 -0400

    created signIn using services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDemoInjectorComponent } from './user-demo/user-demo.injector.component';
+import {UserDemoComponent} from "./user-demo/user-demo.component";
+import {UserService} from "./services/user.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDemoInjectorComponent,
+    UserDemoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
